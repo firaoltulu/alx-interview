bcacbe7e3a1ab8850148a69170b582f9c9b33ece
FiraoolTulu <117259416+firaoltulu@users.noreply.github.com>
2024-05-16 22:09:57 +0300

    my first commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,88 @@
 #!/usr/bin/python3
+"""N queens solution finder module.
+"""
 import sys
 
 
-def n_q(t_arr, arr, col, i, n):
-    """This Function Find all posibles solution for N-queen problem and return it
-    in a list.
+solutions = []
+
+n = 0
+
+pos = None
+
+
+def get_input():
+    """This Function Retrieves and validates this program's argument.
     """
-    if i > n:
-        arr.append(t_arr[:])
-        return arr
-
-    for j in range(n + 1):
-        if i == 0 or (
-            [i - 1, j - 1] not in t_arr and [i - 1, j + 1] not in t_arr and j not in col
-        ):
-            if i > 1:
-                dia = 0
-                for k in range(2, i + 1):
-                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
-                        dia = 1
-                        break
-                if dia:
-                    continue
-            t_arr.append([i, j])
-            col.append(j)
-            n_q(t_arr, arr, col, i + 1, n)
-            col.pop()
-            t_arr.pop()
-
-    return arr
-
-
-if __name__ == "__main__":
+    global n
+    n = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
+        sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except BaseException:
+    except Exception:
         print("N must be a number")
-        exit(1)
-
-    if not isinstance(n, int):
-        print("N must be a number")
-        exit(1)
-
-    elif n < 4:
+        sys.exit(1)
+    if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
+    return n
 
-    n_q_arr = n_q([], [], [], 0, n - 1)
-    for i in n_q_arr:
-        print(i)
+
+def is_attacking(pos0, pos1):
+    """This Function Checks if the positions of two queens are in an attacking mode.
+    """
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
+        return True
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+
+
+def group_exists(group):
+    """This Function Checks if a group exists in the list of solutions.
+    """
+    global solutions
+    for stn in solutions:
+        i = 0
+        for stn_pos in stn:
+            for grp_pos in group:
+                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
+                    i += 1
+        if i == n:
+            return True
+    return False
+
+
+def build_solution(row, group):
+    """This Function Builds a solution for the n queens problem.
+    """
+    global solutions
+    global n
+    if row == n:
+        tmp0 = group.copy()
+        if not group_exists(tmp0):
+            solutions.append(tmp0)
+    else:
+        for col in range(n):
+            a = (row * n) + col
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
+            if not any(used_positions):
+                build_solution(row + 1, group)
+            group.pop(len(group) - 1)
+
+
+def get_solutions():
+    """This Function Gets the solutions for the given chessboard size."""
+    global pos, n
+    pos = list(map(lambda x: [x // n, x % n], range(n**2)))
+    a = 0
+    group = []
+    build_solution(a, group)
+
+
+n = get_input()
+get_solutions()
+for solution in solutions:
+    print(solution)
